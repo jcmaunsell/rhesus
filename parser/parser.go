c637c024e1eac86a3b0639bab139edd27f9260c8
J Maunsell <jcmaunsell@gmail.com>
2022-05-30 10:36:23 -0400

    Clean up

--- parser/parser.go
+++ parser/parser.go
@@ -1,22 +1,26 @@
 package parser
 
 import (
+	"fmt"
 	"github.com/jcmaunsell/rhesus/ast"
 	"github.com/jcmaunsell/rhesus/lexer"
 	"github.com/jcmaunsell/rhesus/token"
+	"github.com/sirupsen/logrus"
+	"go.uber.org/multierr"
 )
 
 type Parser interface {
-	Parse() ast.Program
+	Parse() (ast.Program, error)
 }
 
 type parser struct {
 	lexer               lexer.Lexer
 	curToken, peekToken token.Token
+	log                 *logrus.Logger
 }
 
 func New(lex lexer.Lexer) Parser {
-	p := &parser{lexer: lex}
+	p := &parser{lexer: lex, log: logrus.New()}
 
 	// Initialize curToken and peekToken
 	p.nextToken()
@@ -25,15 +29,20 @@ func New(lex lexer.Lexer) Parser {
 	return p
 }
 
-func (p *parser) Parse() ast.Program {
+func (p *parser) Parse() (ast.Program, error) {
+	var errs error
 	program := ast.Program{}
 	for p.curToken != token.EndOfFile {
-		if statement := p.parseStatement(); statement != nil {
-			program = program.With(statement)
+		statement, err := p.parseStatement()
+		if err != nil {
+			p.log.WithError(err).Error("Could not parse statement.")
+			errs = multierr.Append(errs, err)
+		} else {
+			program = program.Append(statement)
 		}
 		p.nextToken()
 	}
-	return program
+	return program, nil
 }
 
 func (p *parser) nextToken() {
@@ -41,28 +50,33 @@ func (p *parser) nextToken() {
 	p.peekToken = p.lexer.NextToken()
 }
 
-func (p *parser) parseStatement() ast.Statement {
+func (p *parser) parseStatement() (ast.Statement, error) {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLet()
 	default:
-		return nil
+		return nil, fmt.Errorf("unsupported token: %v", p.curToken)
 	}
 }
 
-func (p *parser) parseLet() ast.Statement {
+func (p *parser) parseLet() (ast.Statement, error) {
 	statement := ast.Let{}
+
 	if p.curToken.Type != token.LET {
-		return nil
+		return nil, fmt.Errorf("expected token type '%v', got '%v'", token.LET, p.curToken)
 	}
+	p.nextToken()
+
 	statement.Name = ast.Identifier(p.curToken.Literal)
 	if p.peekToken.Type != token.ASSIGN {
-		return nil
+		return nil, fmt.Errorf("expected token type '%v', got '%v'", token.ASSIGN, p.curToken)
 	}
+	p.nextToken()
+
 	// TODO(jcmaunsell): Parse expression
 	// For now, just skip to the end of the statement
-	for p.curToken.Type != token.Semicolon {
+	for p.curToken.Type != token.SEMICOLON {
 		p.nextToken()
 	}
-	return statement
+	return statement, nil
 }
--- repl/repl.go
+++ repl/repl.go
@@ -4,9 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/jcmaunsell/rhesus/lexer"
+	"github.com/jcmaunsell/rhesus/logger"
+	"github.com/jcmaunsell/rhesus/parser"
 	"github.com/jcmaunsell/rhesus/token"
+	"github.com/sirupsen/logrus"
 	"io"
-	"log"
 )
 
 const PROMPT = "> "
@@ -16,11 +18,12 @@ type REPL interface {
 }
 
 type repl struct {
-	logger *log.Logger
+	log *logrus.Logger
 }
 
-func New(stderr io.Writer) REPL {
-	return &repl{log.New(stderr, "", log.Ldate|log.Ltime|log.Llongfile)}
+func New() (REPL, error) {
+	log := logger.Service()
+	return &repl{log}, nil
 }
 
 func (r *repl) Start(in io.Reader, out io.Writer) {
@@ -28,24 +31,38 @@ func (r *repl) Start(in io.Reader, out io.Writer) {
 
 	for {
 		if _, err := fmt.Fprintf(out, PROMPT); err != nil {
-			r.logger.Println(fmt.Errorf("could not print prompt: %w", err))
+			r.log.Println(fmt.Errorf("could not print prompt: %w", err))
 			return
 		}
 		notDone := scanner.Scan()
 		if !notDone {
 			if err := scanner.Err(); err != nil {
-				r.logger.Println(fmt.Errorf("could not scan input: %w", err))
+				r.log.WithError(err).Println(fmt.Errorf("could not scan input"))
 			}
 			return
 		}
 
-		l := lexer.New(scanner.Text())
+		input := scanner.Text()
+		lex := lexer.New(input)
 
-		for tok := l.NextToken(); tok != token.EndOfFile; tok = l.NextToken() {
+		for tok := lex.NextToken(); tok != token.EndOfFile; tok = lex.NextToken() {
+			if tok.Type == token.ILLEGAL {
+				r.log.WithField("character", tok.Literal).Error("User input contains illegal character.")
+			}
 			if _, err := fmt.Fprintf(out, "%+v\n", tok); err != nil {
-				r.logger.Println(fmt.Errorf("could not print token: %w", err))
+				r.log.WithError(err).WithField("token", tok).Error("Could not print token.")
 				return
 			}
+		}
+
+		program, err := parser.New(lex).Parse()
+		if err != nil {
+			r.log.WithError(err).WithField("input", input).Error("Could not parse program.")
+			return
+		}
+		if _, err := fmt.Fprintf(out, "%+v\n", program); err != nil {
+			r.log.WithError(err).WithField("input", input).Error("Could not print parsed program.")
+			return
 		}
 	}
 }
